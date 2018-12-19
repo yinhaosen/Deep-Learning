95c699bd662dacc8e900e45c3cc494d7bfafd54c
Haosen Yin <yinhaosen@foxmail.com>
2018-12-19 12:11:20 +1100

    finished sklearn calculation

--- TensorFlow/mnist.py
+++ TensorFlow/mnist.py
@@ -1,5 +1,10 @@
 from tensorflow.examples.tutorials.mnist import input_data
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 import tensorflow as tf
+import numpy as np
 
 # %% Initialization
 X    = tf.placeholder(tf.float32, [None, 28, 28, 1])
@@ -15,10 +20,6 @@ Y_ = tf.placeholder(tf.float32, [None, 10])
 
 # %% Loss function
 cross_entropy = -tf.reduce_sum(Y_ * tf.log(Y))
-
-# %% Percentage of correct answers found in batch
-is_correct = tf.equal(tf.argmax(Y,1), tf.argmax(Y_,1))
-accuracy   = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 # %% Training step with the learning rate 0.01
 optimizer  = tf.train.GradientDescentOptimizer(0.01)
@@ -38,8 +39,20 @@ for i in range(10000):
     sess.run(train_step, feed_dict=train_data)
 
     # If success on test data
-    test_X    = mnist.test.images.reshape(-1, 28, 28, 1)
-    test_Y    = mnist.test.labels
-    test_data = {X: test_X, Y_: test_Y}
-    a, c      = sess.run([accuracy, cross_entropy], feed_dict=test_data)
-    print("batch: {}, acc: {}, loss: {}".format(i, a, c))
+    test_X      = mnist.test.images.reshape(-1, 28, 28, 1)
+    test_Y      = mnist.test.labels
+    test_data   = {X: test_X, Y_: test_Y}
+
+    # Percentage of correct answers found in batch called accuracy
+    # Percentage of correctly predicted positive observations to the total predicted positive observations called precision
+    # Percentage of correctly predicted positive observations to the all observations in actual class called recall
+    # F1 Score is the weighted average of precision and recall
+    predicted_Y = sess.run(Y, feed_dict=test_data)
+    predict     = np.argmax(predicted_Y, 1)
+    truth       = np.argmax(test_Y, 1)
+    a           = accuracy_score(truth, predict)
+    p           = precision_score(truth, predict, average=None)
+    r           = recall_score(truth, predict, average=None)
+    f           = f1_score(truth, predict, average=None)
+    c           = sess.run(cross_entropy, feed_dict=test_data)
+    print("batch: {}, acc: {}, pre: {}, re: {}, f1: {}, loss: {}".format(i, a, p, r, f, c))
